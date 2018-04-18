ab3955c66dece241e813374b988191d374514ec6
xpcv <373250051@qq.com>
2018-04-18 11:24:50 +0800

    修改dubbo.xml基于注解的配置 新增swagger对restful接口的支持

--- boot-dubbo-consumer/src/main/java/com/boot/controller/TestController.java
+++ boot-dubbo-consumer/src/main/java/com/boot/controller/TestController.java
@@ -3,6 +3,7 @@ package com.boot.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.boot.domain.User;
 import com.boot.service.TestService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,13 @@ public class TestController {
 
 
     @GetMapping("/hello")
+    @ApiOperation(value = "测试接口",notes = "")
     public String hello(){
         return testService.sayHello("SpringBoot and Dubbo");
     }
 
     @GetMapping("/user")
+    @ApiOperation(value = "获取用户信息",notes = "获取全部用户信息")
     public User user(){
         return testService.findUser();
     }
--- boot-dubbo-provider/src/main/java/com/boot/service/impl/TestServiceImpl.java
+++ boot-dubbo-provider/src/main/java/com/boot/service/impl/TestServiceImpl.java
@@ -3,10 +3,13 @@ package com.boot.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.boot.domain.User;
 import com.boot.service.TestService;
+import org.springframework.stereotype.Component;
 
 
+@Component
 @Service()
 public class TestServiceImpl implements TestService {
+
     @Override
     public String sayHello(String name) {
         return "你好, "+name;
